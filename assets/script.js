ee1aef1b6d8a053063148f295563a48b255c0099
Kevin <kevin@rocher-dev.com>
2023-09-18 15:35:00 +0200

    VMA : add speed

--- assets/script.js
+++ assets/script.js
@@ -24,9 +24,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }());
 
     function speed_to_minutes(value) {
-        var minutes = Math.floor(value);
-        var seconds = Math.round((value - minutes) * 60);
-        return minutes + 'min' + seconds + 's';
+        var hours = Math.floor(value / 60);
+         var minutes = Math.floor(value - (hours * 60));
+         var seconds = Math.round((value - Math.floor(value)) * 60);
+
+         return (hours ? hours + 'h ' : '') + minutes + '&prime;' + seconds + '&Prime;';
     }
 
     (function() {
@@ -57,11 +59,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
             var _speed = 60 / $vma.value;
 
+            /* Aero */
             document.getElementById('form_vma_result_aero').innerHTML = speed_to_minutes(_speed);
-            document.getElementById('form_vma_result_5k').innerHTML = speed_to_minutes(_speed / 0.94);
-            document.getElementById('form_vma_result_10k').innerHTML = speed_to_minutes(_speed / 0.92);
-            document.getElementById('form_vma_result_semi').innerHTML = speed_to_minutes(_speed / 0.86);
-            document.getElementById('form_vma_result_marathon').innerHTML = speed_to_minutes(_speed / 0.82);
+
+            /* 5k */
+            var _speed_5k = _speed / 0.94;
+            document.getElementById('form_vma_result_5k').innerHTML = speed_to_minutes(_speed_5k );
+            document.getElementById('form_vma_result_5k_duration').innerHTML = speed_to_minutes(_speed_5k *5 );
+
+            /* 10k */
+            var _speed_10k = _speed / 0.92;
+            document.getElementById('form_vma_result_10k').innerHTML = speed_to_minutes(_speed_10k);
+            document.getElementById('form_vma_result_10k_duration').innerHTML = speed_to_minutes(_speed_10k * 10);
+
+            /* Semi */
+            var _speed_semi = _speed / 0.86;
+            document.getElementById('form_vma_result_semi').innerHTML = speed_to_minutes(_speed_semi);
+            document.getElementById('form_vma_result_semi_duration').innerHTML = speed_to_minutes(_speed_semi * 21.1);
+
+            /* Marathon */
+            var _speed_mara = _speed / 0.82;
+            document.getElementById('form_vma_result_marathon').innerHTML = speed_to_minutes(_speed_mara);
+            document.getElementById('form_vma_result_marathon_duration').innerHTML = speed_to_minutes(_speed_mara * 42.195);
         }
 
         var _events = ['blur','keyup'];
