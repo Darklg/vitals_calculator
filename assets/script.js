3cb6c577fc12e0d8ba0d9dc01b7285a1182a8abf
Kevin <kevin@rocher-dev.com>
2023-09-18 15:53:18 +0200

    VMA : Vitesses

--- assets/script.js
+++ assets/script.js
@@ -25,20 +25,43 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function speed_to_minutes(value) {
         var hours = Math.floor(value / 60);
-         var minutes = Math.floor(value - (hours * 60));
-         var seconds = Math.round((value - Math.floor(value)) * 60);
-
-         return (hours ? hours + 'h ' : '') + minutes + '&prime;' + seconds + '&Prime;';
+        var minutes = Math.floor(value - (hours * 60));
+        var seconds = Math.round((value - Math.floor(value)) * 60);
+        return (hours ? hours + 'h ' : '') + minutes + '&prime;' + seconds + '&Prime;';
     }
 
     (function() {
         var $vma = document.getElementById('form_vma_vma'),
-            $vo2max = document.getElementById('form_vma_vo2max');
+            $vo2max = document.getElementById('form_vma_vo2max'),
+
+            _ratios = [{
+                    'name': 'aero',
+                    'distance': 1,
+                    'ratio': 1
+                }, {
+                    'name': '5k',
+                    'distance': 5,
+                    'ratio': 0.94
+                }, {
+                    'name': '10k',
+                    'distance': 10,
+                    'ratio': 0.92
+                }, {
+                    'name': 'semi',
+                    'distance': 21.1,
+                    'ratio': 0.86
+                }, {
+                    'name': 'marathon',
+                    'distance': 42.195,
+                    'ratio': 0.82
+                }
+
+            ];
 
         function onchange(e) {
             var $field = e.target;
 
-            if(!$field){
+            if (!$field) {
                 return;
             }
 
@@ -46,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 _vo2max = parseFloat($vo2max.value, 10),
                 _value = parseFloat($field.value.replace(',', '.'));
 
-            if(!_value){
+            if (!_value) {
                 return;
             }
 
@@ -57,33 +80,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 $vma.value = (_value / 3.5).toFixed(2);
             }
 
-            var _speed = 60 / $vma.value;
+            var _vma_value = parseFloat($vma.value);
+            var _speed = 60 / _vma_value;
 
-            /* Aero */
-            document.getElementById('form_vma_result_aero').innerHTML = speed_to_minutes(_speed);
-
-            /* 5k */
-            var _speed_5k = _speed / 0.94;
-            document.getElementById('form_vma_result_5k').innerHTML = speed_to_minutes(_speed_5k );
-            document.getElementById('form_vma_result_5k_duration').innerHTML = speed_to_minutes(_speed_5k *5 );
-
-            /* 10k */
-            var _speed_10k = _speed / 0.92;
-            document.getElementById('form_vma_result_10k').innerHTML = speed_to_minutes(_speed_10k);
-            document.getElementById('form_vma_result_10k_duration').innerHTML = speed_to_minutes(_speed_10k * 10);
-
-            /* Semi */
-            var _speed_semi = _speed / 0.86;
-            document.getElementById('form_vma_result_semi').innerHTML = speed_to_minutes(_speed_semi);
-            document.getElementById('form_vma_result_semi_duration').innerHTML = speed_to_minutes(_speed_semi * 21.1);
-
-            /* Marathon */
-            var _speed_mara = _speed / 0.82;
-            document.getElementById('form_vma_result_marathon').innerHTML = speed_to_minutes(_speed_mara);
-            document.getElementById('form_vma_result_marathon_duration').innerHTML = speed_to_minutes(_speed_mara * 42.195);
+            var _percent_vma,
+                _speed_tmp,
+                _id;
+            for (var i = 0, len = _ratios.length; i < len; i++) {
+                _speed_tmp = _vma_value * _ratios[i].ratio;
+                _percent_vma = _speed / _ratios[i].ratio;
+                _id = 'form_vma_result_' + _ratios[i].name;
+                document.getElementById(_id).innerHTML = speed_to_minutes(_percent_vma);
+                if (document.getElementById(_id + '_speed')) {
+                    document.getElementById(_id + '_speed').innerHTML = (_speed_tmp).toFixed(2);
+                }
+                if (document.getElementById(_id + '_duration')) {
+                    document.getElementById(_id + '_duration').innerHTML = speed_to_minutes(_percent_vma * _ratios[i].distance);
+                }
+            }
         }
 
-        var _events = ['blur','keyup'];
+        var _events = ['blur', 'keyup'];
         for (var i = 0, len = _events.length; i < len; i++) {
             $vma.addEventListener(_events[i], onchange, 1);
             $vo2max.addEventListener(_events[i], onchange, 1);
